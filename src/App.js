a025e8d5b451f3542b5bb2e4046fc697d058634f
AliHaidry <alihaidry11@gmail.com>
2022-06-26 09:01:50 -0400

    code updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import {useEffect} from "react"
 
 // This is called as an functional expression.
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
     const options = {
       method: "GET",
       url: "https://twinword-word-association-quiz.p.rapidapi.com/type1/",
-      params: { level: "3", area: "sat" },
+      params: { level: chosenLevel, area: "sat" },
       headers: {
         "X-RapidAPI-Key": "2d74103165msh55bc962c9e4d63fp1bd12ajsn21a55e1d6325",
         "X-RapidAPI-Host": "twinword-word-association-quiz.p.rapidapi.com",
@@ -28,17 +29,36 @@ const App = () => {
 
   console.log(chosenLevel);
 
+
+  useEffect(() => {
+    if (chosenLevel) getRandomWords()
+  }, [chosenLevel]);
+
   return (
     <div className="App">
-      <select
+
+    {!chosenLevel && <div className="level-selector">
+      <h1>Word Association App</h1>
+      <p>Select Your Level to Start</p>
+    <select
           name="levels"
           id="levels" value={chosenLevel}
           onChange={(e) => setChosenLevel(e.target.value)}
       >
+        <option value={null}>Select a Level</option>
         <option value="1">Level 1</option>
         <option value="2">Level 2</option>
         <option value="3">Level 3</option>
       </select>
+      </div>}
+      
+      {chosenLevel && <div className="question-area">
+        <h1>Welcome to Level: {chosenLevel}</h1>
+      </div>}
+
+    
+
+      
     </div>
   );
 };
